d6099a3bf0c68949d162afdfced57c6b0af0df27
champatiprasanna <prasannachampati88@gmail.com>
2021-09-27 21:45:48 +0530

    closed

---
+++ src/main/java/com/Pojo/Employee.java
@@ -0,0 +1,46 @@
+package com.Pojo;
+
+import java.util.*;
+
+public class Employee {
+   private int id;
+   private String firstName; 
+   private String lastName;   
+   private int salary;
+   
+   
+   public int getId() {
+      return id;
+   }
+   
+   public void setId( int id ) {
+      this.id = id;
+   }
+   
+   public String getFirstName() {
+      return firstName;
+   }
+   
+   public void setFirstName( String first_name ) {
+      this.firstName = first_name;
+   }
+   
+   public String getLastName() {
+      return lastName;
+   }
+   
+   public void setLastName( String last_name ) {
+      this.lastName = last_name;
+   }
+   
+   public int getSalary() {
+      return salary;
+   }
+   
+   public void setSalary( int salary ) {
+      this.salary = salary;
+   }
+
+   
+
+}
