b43dfd2a13e477153d83e365ad0b4bcaa62eee31
champatiprasanna <prasannachampati88@gmail.com>
2021-09-27 16:26:05 +0530

    closed

---
+++ src/main/java/com/Pojo/DetailsPojo.java
@@ -0,0 +1,26 @@
+package com.Pojo;
+
+public class DetailsPojo {
+	private int id;
+	private String location;
+	private String city;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
